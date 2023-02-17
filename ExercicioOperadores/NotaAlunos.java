714c4f5e64ff395947e042a1e78d2902041e095f
Vitorssauro <vitorvtr2003@gmail.com>
2023-02-16 22:01:31 -0300

    16/02/23

---
+++ ExercicioOperadores/NotaAlunos.java
@@ -0,0 +1,20 @@
+package ExercicioOperadores;
+
+import java.util.Scanner;
+
+public class NotaAlunos {
+    Scanner sc = new Scanner(System.in);
+    public void notaFrequencia() {
+        System.out.println("Informe a Nota 1 do Aluno");
+        int nota1 = sc.nextInt();
+        System.out.println("Informe a Nota 2 do Aluno");
+        int nota2 = sc.nextInt();
+        System.out.println("Informe a Frequênciado Aluno");
+        int freq = sc.nextInt();
+        double média = (nota1+nota2)/2;
+        System.out.println("Média do aluno é:" +média);
+        boolean 
+
+    }
+
+}
--- Operacoes/App.java
+++ Operacoes/App.java
@@ -1,3 +1,14 @@
-public static void main(String[] args) {
-    
+package Operacoes;
+
+public class App {
+    public static void main(String[] args) {
+        Operadores objeto1 = new Operadores();
+        objeto1.aritmetica();
+        Operadores objeto2 = new Operadores();
+        objeto2.atribuicao();
+        Operadores objeto3 = new Operadores();
+        objeto3.relacionais();
+        Operadores objeto4 = new Operadores();
+        objeto4.logico();
+    }
 }
