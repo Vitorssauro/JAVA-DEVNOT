f4321055d1dd0bf3f182f90e27b1c29797b3740c
Vitorssauro <vitorvtr2003@gmail.com>
2023-03-16 21:58:43 -0300

    16/02/2023

---
+++ LacoRepeticaoWhile/App.java
@@ -0,0 +1,7 @@
+package LacoRepeticaoWhile;
+
+public class App {
+    public static void main(String[] args) {
+    While.adivinhaNumero();
+    }
+}
---
+++ LacoRepeticaoWhile/While.java
@@ -0,0 +1,33 @@
+package LacoRepeticaoWhile;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class While {
+    public static void adivinhaNumero() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            Random random = new Random();
+            System.out.println("============================");
+            // Aleatórios de 0 até 9
+            int numeroAleatorio = random.nextInt(10);
+            boolean tentarNovamente = true;
+            System.out.println("Aperte CTRL+C, a qualquer momento, paraparar.");
+            // Enquanto tentarNovamente igual a true, itera novamente.
+            while (tentarNovamente) {
+                System.out.print("Tente adivinhar o número: ");
+                int numero = scanner.nextInt();
+                // Repare que a relação abaixo e a de diferença,
+                // enquanto diferente, retorna true e, por isso,
+                // tenta novamente.
+                tentarNovamente = numeroAleatorio != numero;
+                if (tentarNovamente) {
+                    System.out.println("Errado!");
+                }
+            }
+            System.out.println("Parabéns! Você adivinhou. Era o número"
+                    + numeroAleatorio + " mesmo.");
+        }
+        System.out.println("Fim!");
+        System.out.println("============================");
+    }
+}
