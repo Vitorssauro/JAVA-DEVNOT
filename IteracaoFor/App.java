6aa9f9d4dc2e3f5d6dc873cd00557d218707b237
Vitorssauro <vitorvtr2003@gmail.com>
2023-04-18 22:00:26 -0300

    18/04/2023

--- IteracaoFor/App.java
+++ IteracaoFor/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
 
         ExemploFor obj = new ExemploFor();
-        obj.exemplo6();
+        obj.exercicio4Ex();
     }
 }
--- IteracaoFor/ExemploFor.java
+++ IteracaoFor/ExemploFor.java
@@ -53,49 +53,114 @@ public class ExemploFor {
     }
 
     public void exemplo5() {
-        //criando vetor com 10 posições
+        // criando vetor com 10 posições
         double vetorMedia[] = new double[10];
-        //preencher vetor e medias
+        // preencher vetor e medias
         for (int i = 0; i < vetorMedia.length; i++) {
             double notas = 0;
             for (int j = 0; j < 4; j++) {
                 System.out.println("Nota" + (j + 1) + "Aluno" + (i + 1) + ":");
                 notas = sc.nextDouble();
             }
-            notas /= 4;//caluclar média
-            vetorMedia[i] = notas;//colocar média no vetor
+            notas /= 4;// caluclar média
+            vetorMedia[i] = notas;// colocar média no vetor
         }
-        //imprimir média do aluno SE for maior ou igual a 7
+        // imprimir média do aluno SE for maior ou igual a 7
         for (int i = 0; i < vetorMedia.length; i++) {
-            if (vetorMedia[i]>= 7) {
-                System.out.println("Média do Aluno"+(i+1)+"é "+vetorMedia[i] );
+            if (vetorMedia[i] >= 7) {
+                System.out.println("Média do Aluno" + (i + 1) + "é " + vetorMedia[i]);
             }
         }
     }
 
     public void exemplo6() {
-        int vetorNumeros[] = new int [5];
+        int vetorNumeros[] = new int[5];
         for (int i = 0; i < vetorNumeros.length; i++) {
-            System.out.println("Informe o "+(i+1)+"º nº:");
-            vetorNumeros[i]=sc.nextInt();
+            System.out.println("Informe o " + (i + 1) + "º nº:");
+            vetorNumeros[i] = sc.nextInt();
         }
         int soma = 0;
         for (int i = 0; i < vetorNumeros.length; i++) {
             soma += vetorNumeros[i];
         }
-        System.out.println("Soma = "+soma);
+        System.out.println("Soma = " + soma);
         int multi = 1;
         for (int i = 0; i < vetorNumeros.length; i++) {
             multi *= vetorNumeros[i];
         }
-        System.out.println("Multipilcação = "+multi);
+        System.out.println("Multipilcação = " + multi);
         System.out.println("os números são:");
         for (int i = 0; i < vetorNumeros.length; i++) {
-            System.out.println((i+1)+" ºn = "+vetorNumeros[i] );
+            System.out.println((i + 1) + " ºn = " + vetorNumeros[i]);
         }
     }
+
     public void exemplo7() {
         String pessoas[] = new String[5];
+        int idade = 0;
+        for (int i = 0; i < pessoas.length; i++) {
+            System.out.println("Informe o nome da " + (i + 1) + "ª pessoa");
+
+            for (int j = 0; j < 1; j++) {
+                System.out.println("Digite sua idade:");
+                idade = sc.nextInt();
+            }
+        }
+
     }
 
+    public void exercicio4() {
+        // char letrasC[] = new char []{'a','b','c','d','e','f','g','h','i','j'};
+        String lestras[] = new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
+        int cont = 0;
+        for (int i = 0; i < lestras.length; i++) {
+            if (lestras[i] != "a" && lestras[i] != "e" && lestras[i] != "i" && lestras[i] != "o" && lestras[i] != "u") {
+                System.out.println(lestras[i] + "é consoante");
+                cont++;
+            }
+        }
+        System.out.println("O nº de consoantes é " + cont);
+    }
+
+    public void exercicio4Ex() {
+        System.out.println("Digite uma palavra:");
+        String palavra = sc.next();
+        palavra = palavra.toLowerCase();
+        int cont = 0;
+        for (int i = 0; i < palavra.length(); i++) {
+            char c = palavra.charAt(i);
+
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                System.out.println(c + " é consoante");
+                cont++;
+            }
+        }
+        System.out.print("O nº de consoantes é " + cont);
+    }
+
+    public void exercicio5() {
+        // criar vetor para os numeros
+        int numeros[] = new int[20];
+        int contPar=0; //contador de nmrs pares
+        int contImpar=0; //contador de nmrs impares
+        //Preencher o vetor de numeros e determinar o nmr de imapres e pares
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("informe o nº ["+i+"]=");
+            numeros[i]=sc.nextInt();
+            if (numeros[i]%2==0) {
+                contPar++;
+            } else {
+                contImpar++;
+            }
+        }
+        //apos a contagem de pares e impares criar seus vetores
+        int nPar[]= new int[contPar];
+        int nImpar[] = new int[contImpar];
+        //distribuir os nº de vetores para par e impar
+        contPar = 0;
+        contImpar = 0;
+        for (int i = 0; i < nPar.length; i++) {
+            
+        }
+    }
 }
