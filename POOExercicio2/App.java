938a51a0250699d7bf565b8f0334c988401c10d4
Vitorssauro <vitorvtr2003@gmail.com>
2023-06-06 21:13:23 -0300

    06/06/2023

--- POOExercicio2/App.java
+++ POOExercicio2/App.java
@@ -1,21 +1,24 @@
 package POOExercicio2;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Random rd = new Random();
         Scanner sc = new Scanner(System.in);
         // vetor de objetos
         Agenda pessoas[] = new Agenda[10];
+        
         // preencher o meu vetor/Array
         for (int i = 0; i < pessoas.length; i++) {
             // construtor de objetos
             pessoas[i] = new Agenda();
             // preencher atributos
-            pessoas[i].setNome("Pessoa " + i);
-            pessoas[i].setAltura(i);
+            pessoas[i].setNome("Pessoa " + (i + 1));
+            pessoas[i].setAltura(rd.nextDouble());
             pessoas[i].setIdade(i + 18);
-            System.out.println("---------");
+            System.out.println("------------");
             System.out.println("Nome: " + pessoas[i].getNome()
                     + " Idade: " + pessoas[i].getIdade()
                     + " Altura: " + pessoas[i].getAltura());
@@ -30,8 +33,8 @@ public class App {
         System.out.println("Informe o nome que procura: ");
         String nomeDigitado = sc.nextLine();
 
-        while(!nomeDigitado.equals(pessoas[cont].getNome())){
-                cont++;
+        while (!nomeDigitado.equals(pessoas[cont].getNome())) {
+            cont++;
         }
         System.out.println(" Nome: " + pessoas[cont].getNome()
                 + " Idade: " + pessoas[cont].getIdade()
